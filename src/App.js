b54df60a76900b2de0855409b74cd97f9826cd89
Victor Martins <vhprestes@gmail.com>
2022-05-12 18:01:32 -0300

    Requisito 2 pronto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import Form from './components/Form';
 
+// const initialState = {
+//   name: '',
+//   description: '',
+//   attr1: 0,
+//   attr2: 0,
+//   attr3: 0,
+//   image: '',
+//   rare: '',
+//   hasTrunfo: false,
+//   isSaveButtonDisabled: false,
+//   cardTrunfo: false,
+// }
+
 class App extends React.Component {
+  state = {
+    name: '',
+    description: '',
+    attr1: 0,
+    attr2: 0,
+    attr3: 0,
+    image: '',
+    rare: '',
+    hasTrunfo: false,
+    isSaveButtonDisabled: false,
+    cardTrunfo: false,
+  }
+
+  handleChange = (target) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  };
+
+  onSaveButtonClick = () => {}
+
   render() {
+    const { name, description, attr1, attr2, attr3, image, rare,
+      hasTrunfo, cardTrunfo, isSaveButtonDisabled } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
+        <Form
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+          cardImage={ image }
+          cardRare={ rare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.handleChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
       </div>
     );
   }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const { cardAttr1, cardAttr2, cardAttr3, cardName, cardImage, cardDescription,
+      cardRare, cardTrunfo, isSaveButtonDisabled, onInputChange,
+      onSaveButtonClick } = this.props;
     return (
       <form>
         <label htmlFor="name-input">
           Nome:
           {/* https://stackoverflow.com/questions/62306461/label-must-have-associated-control */}
-          <input id="name-input" data-testid="name-input" type="text" />
+          <input
+            id="name-input"
+            data-testid="name-input"
+            type="text"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="description-input">
           Descrição:
@@ -15,27 +25,58 @@ class Form extends React.Component {
             id="description-input"
             data-testid="description-input"
             type="text"
+            value={ cardDescription }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="attr1">
           Atributo 1
-          <input type="number" data-testid="attr1-input" name="attr1" />
+          <input
+            type="number"
+            data-testid="attr1-input"
+            name="attr1"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="attr1">
           Atributo 2
-          <input type="number" data-testid="attr2-input" name="attr2" />
+          <input
+            type="number"
+            data-testid="attr2-input"
+            name="attr2"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="attr1">
           Atributo 3
-          <input type="number" data-testid="attr3-input" name="attr3" />
+          <input
+            type="number"
+            data-testid="attr3-input"
+            name="attr3"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="image">
           Image
-          <input type="text" data-testid="image-input" name="image" />
+          <input
+            type="text"
+            data-testid="image-input"
+            name="image"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="rare">
           Rarity:
-          <select data-testid="rare-input" name="rare">
+          <select
+            data-testid="rare-input"
+            name="rare"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
             <option value="normal">normal</option>
             <option value="raro">raro</option>
             <option value="muito raro">muito raro</option>
@@ -43,14 +84,40 @@ class Form extends React.Component {
         </label>
         <label htmlFor="trunfo">
           Trunfo:
-          <input type="checkbox" data-testid="trunfo-input" name="trunfo" />
+          <input
+            type="checkbox"
+            data-testid="trunfo-input"
+            name="trunfo"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
         </label>
-        <button type="button" data-testid="save-button">
+        <button
+          type="button"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
           Salvar
         </button>
       </form>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.number.isRequired,
+  cardAttr2: PropTypes.number.isRequired,
+  cardAttr3: PropTypes.number.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
